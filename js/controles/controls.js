27f31238bca8a28445eab00094e96997adf4e26e
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-06-20 20:20:52 -0300

    Adicionado prototypes para deixar codigo mais curto

--- js/controles/controls.js
+++ js/controles/controls.js
@@ -2,9 +2,9 @@
 function getStyle(element,value){
     return window.getComputedStyle(element).getPropertyValue(value)
 }
-const colorDefault = getStyle(document.querySelector(".btn-control"),"background-color")
+const colorDefault = getStyle($(".btn-control")[0],"background-color")
 function getBtn(){
-    document.querySelectorAll(".btn-control").forEach(btn=>{
+    $(".btn-control").forEach(btn=>{
         getStyle(btn,"background-color")!=colorDefault?console.log(btn.id):0//Aqui coloca a função ou método que realiza a movimentação do player
     })
     
